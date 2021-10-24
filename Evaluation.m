f34cac2d4becb23a974f2db9a29b90682046539f
Sowmya Buddharaju <70256420+sowmya214@users.noreply.github.com>
2021-10-24 17:13:59 +0100

    Update Evaluation.m

--- Evaluation.m
+++ Evaluation.m
@@ -1,23 +1,10 @@
 clear all;
 close all;
-trainPath='C:\university\compvision\CWMaterial\FaceDatabase\Train\';
-testPath='C:\university\compvision\CWMaterial\FaceDatabase\Test\';
+trainPath='FaceDatabase\Train\';
+testPath='FaceDatabase\Test\';
 
-%% Baseline Method
-tic;
-   outputLabel=FaceRecognition(trainPath, testPath);
-baseLineTime=toc
 
-load testLabel
-correctP=0;
-for i=1:size(testLabel,1)
-    if strcmp(outputLabel(i,:),testLabel(i,:))
-        correctP=correctP+1;
-    end
-end
-recAccuracy=correctP/size(testLabel,1)*100  %Recognition accuracy%
-
-%% Method 1 developed by you
+%% Method 1 - HOG Features + SVM
 tic;
    outputLabel1=FaceRecognition1(trainPath, testPath);
 method1Time=toc
@@ -33,7 +20,7 @@ method1Time=toc
  recAccuracy=correctP/size(testLabel,1)*100  %Recognition accuracy%
 
 
-%% Method 2 developed by you
+%% Method 2 - Transfer Learning with AlexNet
 tic;
    outputLabel2=FaceRecognition2(trainPath, testPath);
 method2Time=toc
